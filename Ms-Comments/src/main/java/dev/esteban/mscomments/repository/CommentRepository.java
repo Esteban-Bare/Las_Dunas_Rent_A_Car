2c9335923cd6515b96f6b5c16a7e39868a4c1a50
Esteban <150131639+Esteban-Bare@users.noreply.github.com>
2025-04-22 13:47:54 +0200

    #6 Add Comment model, repository, and application setup for comments service

---
+++ Ms-Comments/src/main/java/dev/esteban/mscomments/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package dev.esteban.mscomments.repository;
+
+import dev.esteban.mscomments.model.Comment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends MongoRepository<Comment, Long> {
+    List<Comment> findCommentsByVehicleId(String vehicleId);
+
+    Comment findById(String commentId);
+
+    void deleteById(String commentId);
+}
