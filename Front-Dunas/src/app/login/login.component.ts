0bad6e71e8f0fe7bdff800b8085f07aef5d583c9
Esteban <150131639+Esteban-Bare@users.noreply.github.com>
2025-04-29 09:39:27 +0200

    #12 Add initial implementation of comments and pricing microservices with basic setup

---
+++ Front-Dunas/src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { Credentials } from '../interface/credentials'
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  constructor(public AuthService: AuthService,private router:Router) {}
+
+  loginForm = new FormGroup({
+    email: new FormControl<string>("", [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl<string>("",[
+      Validators.required,
+      Validators.minLength(6)
+    ])
+  })
+
+  public async login(): Promise<void> {
+    try {
+      const email = this.loginForm.get('email')?.value!;
+      const password = this.loginForm.get('password')?.value!;
+      let credentials: Credentials = {
+        email: email,
+        password: password
+      }
+      await this.AuthService.login(credentials);
+      console.log("Login successful");
+      this.router.navigate(['/dashboard']);
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  }
+}
---
+++ Ms-Comments/src/main/java/dev/esteban/mscomments/MsCommentsApplication.java
@@ -0,0 +1,33 @@
+package dev.esteban.mscomments;
+
+import dev.esteban.mscomments.model.Comment;
+import dev.esteban.mscomments.repository.CommentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+
+@SpringBootApplication
+public class MsCommentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCommentsApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(CommentRepository commentRepository) {
+        return args -> {
+            commentRepository.deleteAll();
+            Comment comment1 = new Comment("1","2", 4, "Comment 1", LocalDate.now().toString());
+            Comment comment2 = new Comment("2","3", 1, "Comment 2", LocalDate.now().toString());
+            Comment comment2v2 = new Comment("2","1", 3, "Comment 2", LocalDate.now().toString());
+            Comment comment3 = new Comment("3","1", 5, "Comment 3", LocalDate.now().toString());
+            commentRepository.save(comment1);
+            commentRepository.save(comment2);
+            commentRepository.save(comment2v2);
+            commentRepository.save(comment3);
+        };
+    }
+}
