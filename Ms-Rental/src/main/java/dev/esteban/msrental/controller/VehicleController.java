e6f8f7885a3144dbf4b812a09a6c060cea74b702
Esteban <150131639+Esteban-Bare@users.noreply.github.com>
2025-09-05 15:46:41 +0200

    #21 Change HTTP methods in PricingController and VehicleController to use POST for price retrieval and available cars

--- Ms-Rental/src/main/java/dev/esteban/msrental/controller/VehicleController.java
+++ Ms-Rental/src/main/java/dev/esteban/msrental/controller/VehicleController.java
@@ -29,7 +29,7 @@ public class VehicleController {
         return vehicleRepository.findById(id).orElse(null);
     }
 
-    @GetMapping("/available")
+    @PostMapping("/available")
     public ResponseEntity<?> getAvailableCars(@RequestBody VehicleSearchDto vehicleSearchDto) {
         return vehicleService.getAvailableCars(vehicleSearchDto);
     }
